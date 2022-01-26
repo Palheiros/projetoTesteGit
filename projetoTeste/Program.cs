65ef4acaa3587d52fc314ef949cf79e6ad50311c
Palheiros <ricardopalheiros@gmail.com>
2022-01-25 22:53:40 -0300

    Teste do Git Bash.

---
+++ projetoTeste/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace projetoTeste
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o seu nome: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Olá, " + nome +"!");
+            Console.ReadLine();
+        }
+    }
+}
