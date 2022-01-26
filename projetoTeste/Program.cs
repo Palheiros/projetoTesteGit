2ff1d77d3eab9cd7624be3ce012eff255fd79b1a
Palheiros <ricardopalheiros@gmail.com>
2022-01-25 23:32:30 -0300

    Acrescentado condicional IF/Else

--- projetoTeste/Program.cs
+++ projetoTeste/Program.cs
@@ -9,12 +9,25 @@ namespace projetoTeste
             Console.Write("Digite o seu nome: ");
             string nome = Console.ReadLine();
             Console.WriteLine();
+
             Console.WriteLine("Olá, " + nome +"!");
+            
             Console.Write("Quantos anos você tem? ");
             int idade = int.Parse(Console.ReadLine());
             Console.WriteLine();
+            
             int dias = idade * 365;
             Console.WriteLine(nome + ", você já viveu aproximadamente " + dias + " dias!");
+            Console.WriteLine();
+
+            if (idade < 18)
+            {
+                Console.WriteLine("Usuário menor de idade. Acesso não autorizado!");
+            } else
+            {
+                Console.WriteLine("Maior de idade. Pode prosseguir com o login.");
+            }
+            
             Console.WriteLine();
             Console.WriteLine("Tenha um bom dia!");
 
