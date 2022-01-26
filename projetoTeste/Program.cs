6d98f2a2fa5e51c02b21eb98b0b99aa95084ac05
Palheiros <ricardopalheiros@gmail.com>
2022-01-26 08:55:31 -0300

    Eliminei as concatenações nos outputs

--- projetoTeste/Program.cs
+++ projetoTeste/Program.cs
@@ -10,14 +10,14 @@ namespace projetoTeste
             string nome = Console.ReadLine();
             Console.WriteLine();
 
-            Console.WriteLine("Olá, " + nome +"!");
+            Console.WriteLine($"Olá, {nome}!");
             
             Console.Write("Quantos anos você tem? ");
             int idade = int.Parse(Console.ReadLine());
             Console.WriteLine();
             
             int dias = idade * 365;
-            Console.WriteLine(nome + ", você já viveu aproximadamente " + dias + " dias!");
+            Console.WriteLine($"{nome}, você já viveu aproximadamente {dias} dias!");
             Console.WriteLine();
 
             if (idade < 18)
